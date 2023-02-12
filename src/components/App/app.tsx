47040a5fd4f28415ebdded0376c6978fce01bcc8
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-02-12 17:52:07 -0300

    commit 120220231752

---
+++ src/components/App/app.tsx
@@ -0,0 +1,13 @@
+import { Cube } from "../Cube/cube";
+import './style.css'
+
+export function App() {
+
+  return (
+    <div className="app">
+      <Cube/>
+    </div>
+  );
+}
+
+
---
+++ src/components/Cube/cube.tsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export function Cube() {
+  return (
+    <div className="cube">
+      
+    </div>
+  );
+}
