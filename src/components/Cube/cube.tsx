ff87588b3bd34e8b6945c9fa2bb7ed0e5fae7f4c
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-02-23 01:07:28 -0300

    commit 230220230107

--- src/components/Cube/cube.tsx
+++ src/components/Cube/cube.tsx
@@ -5,6 +5,8 @@ import { positions, colors } from "../Configs/configs";
 import { useEffect, useRef, useState } from "react";
 
 import { randInt } from "three/src/math/MathUtils";
+import { cube1x1Props } from "../../types/types";
+import { Line } from "three";
 
 export function Cube() {
   const ref = useRef<THREE.Mesh>(null!);
@@ -16,17 +18,52 @@ export function Cube() {
     return `#${randInt(100, 999)}`;
   }
 
-  const atmCube = (
+  const defaultColors = ["black", "black", "black", "black", "black", "black"];
+
+  function cube1x1({
+    x = 0,
+    y = 0,
+    z = 0,
+    colors = defaultColors,
+  }: cube1x1Props) {
+    return (
+      <>
+        <mesh position={[x + 0, y + 0, z + 1]}>          
+          <boxGeometry args={[0, 1, 1]} />          
+          <meshStandardMaterial color={'black'} />
+        </mesh>
+        <mesh position={[x + 1, y + 0, z + 1]}>
+          <boxGeometry args={[0, 1, 1]} />
+          <meshStandardMaterial color={colors[randInt(0,5)]} />
+        </mesh>
+        <mesh position={[x + 0.5, y + 0, z + 0.5]}>
+          <boxGeometry args={[1, 1, 0]} />
+          <meshStandardMaterial color={colors[randInt(0,5)]} />
+        </mesh>
+        <mesh position={[x + 0.5, y + 0, z + 1.5]}>
+          <boxGeometry args={[1, 1, 0]} />
+          <meshStandardMaterial color={colors[randInt(0,5)]} />
+        </mesh>
+        <mesh position={[x + 0.5, y + -0.5, z + 1]}>
+          <boxGeometry args={[1, 0, 1]} />
+          <meshStandardMaterial color={colors[randInt(0,5)]} />
+        </mesh>
+        <mesh position={[x + 0.5, y + 0.5, z + 1]}>
+          <boxGeometry args={[1, 0, 1]} />
+          <meshStandardMaterial color={colors[randInt(0,5)]} />
+        </mesh>
+      </>
+    );
+  }
+
+  const cores = ["red", "green", "blue", "yellow", "white", "red"];
+
+  const cube3x3 = (
     <>
       <OrbitControls />
       <mesh position={[-1.5, -1.5, -1.5]} rotation={[degX, degY, degZ]}>
         {positions.map((pos, i) => {
-          return (
-            <mesh position={[pos[0], pos[1], pos[2]]}>
-              <boxGeometry args={[1, 1, 1]} />
-              <meshStandardMaterial color={colors[i]} />
-            </mesh>
-          );
+          return cube1x1({ x: pos[0], y: pos[1], z: pos[2], colors: cores });
         })}
       </mesh>
     </>
@@ -34,12 +71,24 @@ export function Cube() {
 
   useEffect(() => {}, [degX, degY, degZ]);
 
+  const propsCube1: cube1x1Props = {
+    x: 3,
+    y: 3,
+    z: 1,
+    colors: ["red", "green", "black", "yellow", "orange", "red"],
+  };
+
   return (
     <div className="cube">
       <Canvas className="canva">
         <ambientLight />
-        <pointLight position={[20, 20, 10]} />
-        {atmCube}
+        <pointLight position={[10, 20, 10]} />
+        {cube3x3}
+        <mesh position={[3, 3, 1]}>          
+          <boxGeometry args={[1, 1, 0,1,1,0]} />
+          
+          <meshStandardMaterial color={'black'} wireframe={true} wireframeLinewidth={5} />
+        </mesh>
       </Canvas>
     </div>
   );
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+import { Color } from "three/src/math/Color"
+
+export type cube1x1Props = {
+  x?: number,
+  y?: number,
+  z?: number,
+  colors?: Color[] | string[]
+}
