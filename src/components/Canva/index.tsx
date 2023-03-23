5a6e077a452d23e2b9d64fba25998e077aede2c5
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-03-22 23:26:47 -0300

    commit 220320232326

--- src/components/Canva/index.tsx
+++ src/components/Canva/index.tsx
@@ -1,18 +1,43 @@
 import { useEffect, useRef, useState } from "react";
+import {TCube, TPoint2D } from '../../types/types'
 import "./style.css";
+
+function Cube<TCube>(points: TPoint2D, size: number) {
+  const x = points.x;
+  const y = points.y;
+  const s = size;
+  return {
+    p000: {x:x,y:y,z:0},
+    p100: {x: x+s, y,0},
+    p110: {x: x+s, y+s,0},
+    p010: {x: x,y+s,0},
+    p011: {x: x,y+s,s},
+    p111: {x: x+s,y+s,s},
+    p101: {x: x+s,y,s},
+    p001: {x: x,y,s},
+}
+}
+
 
 export function Canva() {
   const refCanva = useRef<HTMLCanvasElement>(null);
-  const [gl, setGl] = useState<WebGLRenderingContext | null>()
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>();
   useEffect(() => {
-    setGl(refCanva.current?.getContext('webgl'))
+    setCtx(refCanva.current?.getContext("2d"));
   }, []);
-  
-  useEffect(() => {
-    gl?.viewport(0, 0, 500, 500);
-    gl?.enable(gl.DEPTH_TEST);
 
-    
+  
+
+  const cube:TCube = {
+    { }, { }, { }, { }, { }, { }, { }, { }
+  }
+
+  useEffect(() => {
+    if (ctx) {
+      ctx.fillStyle;
+      ctx.fillStyle = "red";
+      ctx.fillRect(0, 0, 40, 50);
+    }
   }, []);
 
   return <canvas className="canva" ref={refCanva}></canvas>;
--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,23 @@ export type cube1x1Props = {
   z?: number,
   colors?: Color[] | string[]
 }
+
+export type TPoint2D = {
+  x: number,
+  y: number,
+}
+
+export type TPoint3D = TPoint2D &{
+  z: number,
+}
+
+export type TCube = {
+  p000: TPoint3D,
+  p100: TPoint3D,
+  p110: TPoint3D,
+  p010: TPoint3D,
+  p011: TPoint3D,
+  p111: TPoint3D,
+  p101: TPoint3D,
+  p001: TPoint3D,
+}
