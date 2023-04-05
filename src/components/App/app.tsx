d1e922453a9508aa615833d9f9f656f58bc09626
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-04-05 00:10:05 -0300

    commit 050420230010

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,11 +1,62 @@
+import { useEffect, useRef } from "react";
 import { Cube } from "../Cube/cube";
 import './style.css'
+import * as THREE from "three";
 
 export function App() {
 
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.z = 96;
+
+    const canvas = document.getElementById('myThreeJsCanvas') as HTMLCanvasElement;
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement)
+      
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    ambientLight.castShadow = true;
+    scene.add(ambientLight);
+
+    const spotLight = new THREE.SpotLight(0xffffff, 1);
+    spotLight.castShadow = true;
+    spotLight.position.set(0, 64, 32);
+    scene.add(spotLight);
+
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    scene.add(boxMesh);
+
+
+    const animate = () => {
+
+      boxMesh.rotation.x += 0.05;
+      // boxMesh.rotation.y += 0.05;
+      // boxMesh.rotation.z -= 0.05;
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(animate);
+    }
+    animate();
+    console.log('okok');
+
+  }, []);
+
   return (
-    <div className="app">
-      <Cube/>
+    <div>
+     <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
--- src/components/Cube/cube.tsx
+++ src/components/Cube/cube.tsx
@@ -1,103 +1,49 @@
 import "./style.css";
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import {
-  BufferGeometryProps,
-  MeshProps,
-  ThreeElements,
-} from "@react-three/fiber/dist/declarations/src";
-import { useEffect, useRef, useState } from "react";
-import { BufferGeometry, Euler, Line, LineBasicMaterial, Material, Mesh, Vector3 } from "three";
-import { randInt } from "three/src/math/MathUtils";
+
+import { useRef, useState } from "react";
+import THREE, { BufferGeometry, Mesh, MeshBasicMaterial, rotateInX } from "three";
 
 export function Cube() {
-  const ref = useRef<THREE.Mesh>(null!);
+  const cubeRef = useRef<THREE.Mesh>(null!);
   const [degX, setDegX] = useState<number>(0);
   const [degY, setDegY] = useState<number>(0);
   const [degZ, setDegZ] = useState<number>(0);
 
-  function aleatoryColor() {
-    return `#${randInt(100, 999)}`;
+  type angle = {
+    angle: number
   }
 
-  const positions = [
-    [0, 0, 0],
-    [1, 0, 0],
-    [2, 0, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-    [2, 1, 0],
-    [0, 2, 0],
-    [1, 2, 0],
-    [2, 2, 0],
-    [0, 0, 1],
-    [1, 0, 1],
-    [2, 0, 1],
-    [0, 1, 1],
-    [1, 1, 1],
-    [2, 1, 1],
-    [0, 2, 1],
-    [1, 2, 1],
-    [2, 2, 1],
-    [0, 0, 2],
-    [1, 0, 2],
-    [2, 0, 2],
-    [0, 1, 2],
-    [1, 1, 2],
-    [2, 1, 2],
-    [0, 2, 2],
-    [1, 2, 2],
-    [2, 2, 2],
-  ];
+  const r:angle = {angle: 0.01}
 
-  const atmCube = (
-    <>
-      
-      <OrbitControls autoRotate />
-      <mesh position={[-1.5,-1.5,-1.5]}  rotation={[degX, degY, degZ]}>
+  // function animate() {
+  //   cubeRef.current.rotateX = (angle) => {
+  //     return (
+  //       <BufferGeometry >
 
-      {positions.map((pos) => {
-        return (
-          <mesh
-          position={[pos[0], pos[1], pos[2]]}
-          
-          >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={aleatoryColor()} />
-          </mesh>
-        );
-      })}
-      </mesh>
-    </>
+  //       </BufferGeometry>
+  //     )
+  //   }; 
+  //   requestAnimationFrame(() => animate());
+  // }
+
+  // useEffect(() => {
+  //   animate();
+  // }, []);
+
+  const cube = (
+    <Box ref={cubeRef} args={[2,2,2,1,1,5]}>
+
+    </Box>
   );
-
-  useEffect(() => {}, [degX, degY, degZ]);
 
   return (
     <div className="cube">
       <Canvas className="canva">
-        <ambientLight />
-        <pointLight position={[20, 20, 10]} />
-        {atmCube}
+        <OrbitControls/>
+        {cube}
       </Canvas>
-      <input
-        type="range"
-        min={0}
-        max={50}
-        onChange={(e) => setDegX(Number(e.target.value))}
-      />
-      <input
-        type="range"
-        min={0}
-        max={50}
-        onChange={(e) => setDegY(Number(e.target.value))}
-      />
-      <input
-        type="range"
-        min={0}
-        max={50}
-        onChange={(e) => setDegZ(Number(e.target.value))}
-      />
     </div>
   );
 }
